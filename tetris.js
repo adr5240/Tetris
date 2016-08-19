9e1ba076bcd308b579cd8c8e7cb96cfbc48723e0
Adam Reiter <adr5240@gmail.com>
2016-08-18 23:38:09 -0400

    Add rotate method

--- tetris.js
+++ tetris.js
@@ -1,6 +1,5 @@
 let tetris = {};
 tetris.origin = { row: 5, col: 5 };
-tetris.currentShape = 'J';
 
 
 tetris.drawPlayField = function () {
@@ -42,6 +41,12 @@ tetris.move = function(direction){
         }
     }
 
+    if (direction === 'right') {
+        this.origin.col++;
+    } else if (direction === 'left') {
+        this.origin.col--;
+    }
+
     this.fillCells(this.currentCoor, 'black');
 
     if(reverse && direction === 'left'){
@@ -55,10 +60,37 @@ tetris.move = function(direction){
 tetris.shapeToCoor = function (shape, origin) {
     if (shape === 'L') {
         return [
-                { row: origin.row, col: origin.col -1 },
+                { row: origin.row, col: origin.col - 1 },
                 { row: origin.row, col: origin.col },
                 { row: origin.row, col: origin.col + 1 },
                 { row: origin.row - 1, col: origin.col + 1 }
+        ];
+    }
+
+    if (shape === "L90") {
+        return [
+            { row: origin.row + 1, col: origin.col },
+            { row: origin.row, col: origin.col },
+            { row: origin.row - 1, col: origin.col },
+            { row: origin.row - 1, col: origin.col - 1 }
+        ];
+    }
+
+    if (shape === "L180") {
+        return [
+            { row: origin.row, col: origin.col + 1 },
+            { row: origin.row, col: origin.col },
+            { row: origin.row, col: origin.col - 1 },
+            { row: origin.row + 1, col: origin.col - 1 }
+        ];
+    }
+
+    if (shape === "L270") {
+        return [
+            { row: origin.row - 1, col: origin.col },
+            { row: origin.row, col: origin.col },
+            { row: origin.row + 1, col: origin.col },
+            { row: origin.row + 1, col: origin.col + 1 }
         ];
     }
 
@@ -71,12 +103,48 @@ tetris.shapeToCoor = function (shape, origin) {
         ];
     }
 
+    if (shape === 'J90') {
+        return [
+                { row: origin.row - 1, col: origin.col },
+                { row: origin.row, col: origin.col },
+                { row: origin.row + 1, col: origin.col },
+                { row: origin.row + 1, col: origin.col - 1 }
+        ];
+    }
+
+    if (shape === 'J180') {
+        return [
+                { row: origin.row, col: origin.col - 1 },
+                { row: origin.row, col: origin.col },
+                { row: origin.row, col: origin.col + 1 },
+                { row: origin.row + 1, col: origin.col + 1 }
+        ];
+    }
+
+    if (shape === 'J270') {
+        return [
+                { row: origin.row + 1, col: origin.col },
+                { row: origin.row, col: origin.col },
+                { row: origin.row - 1, col: origin.col },
+                { row: origin.row - 1, col: origin.col + 1 }
+        ];
+    }
+
     if (shape === 'I') {
         return [
                 { row: origin.row, col: origin.col - 1 },
                 { row: origin.row, col: origin.col },
                 { row: origin.row, col: origin.col + 1 },
                 { row: origin.row, col: origin.col + 2 }
+        ];
+    }
+
+    if (shape === 'I90') {
+        return [
+                { row: origin.row - 1, col: origin.col },
+                { row: origin.row, col: origin.col },
+                { row: origin.row + 1, col: origin.col },
+                { row: origin.row + 2, col: origin.col }
         ];
     }
 
@@ -98,6 +166,33 @@ tetris.shapeToCoor = function (shape, origin) {
         ];
     }
 
+    if (shape === 'S90') {
+        return [
+                { row: origin.row - 1, col: origin.col },
+                { row: origin.row, col: origin.col },
+                { row: origin.row, col: origin.col + 1 },
+                { row: origin.row + 1, col: origin.col + 1 }
+        ];
+    }
+
+    if (shape === 'Z') {
+        return [
+            { row: origin.row, col: origin.col - 1 },
+            { row: origin.row, col: origin.col },
+            { row: origin.row + 1, col: origin.col },
+            { row: origin.row + 1, col: origin.col + 1 }
+        ];
+    }
+
+    if (shape === 'Z90') {
+        return [
+            { row: origin.row + 1, col: origin.col },
+            { row: origin.row, col: origin.col },
+            { row: origin.row, col: origin.col + 1 },
+            { row: origin.row - 1, col: origin.col + 1 }
+        ];
+    }
+
     if (shape === 'T') {
         return [
                 { row: origin.row - 1, col: origin.col },
@@ -107,16 +202,101 @@ tetris.shapeToCoor = function (shape, origin) {
         ];
     }
 
-    if (shape === 'Z') {
+    if (shape === 'T90') {
         return [
-                { row: origin.row, col: origin.col - 1 },
+                { row: origin.row - 1, col: origin.col },
                 { row: origin.row, col: origin.col },
                 { row: origin.row + 1, col: origin.col },
-                { row: origin.row + 1, col: origin.col + 1 }
+                { row: origin.row, col: origin.col - 1}
+        ];
+    }
+
+    if (shape === 'T180') {
+        return [
+                { row: origin.row + 1, col: origin.col },
+                { row: origin.row, col: origin.col },
+                { row: origin.row, col: origin.col - 1},
+                { row: origin.row, col: origin.col + 1}
+        ];
+    }
+
+    if (shape === 'T270') {
+        return [
+                { row: origin.row - 1, col: origin.col },
+                { row: origin.row, col: origin.col },
+                { row: origin.row + 1, col: origin.col },
+                { row: origin.row, col: origin.col + 1}
         ];
     }
 };
 
+
+tetris.rotate = function () {
+    let prevShape = this.currentShape;
+    this.fillCells(this.currentCoor, '');
+
+    if (this.currentShape === 'L') {
+        this.currentShape = 'L90';
+    } else if (this.currentShape === 'L90') {
+        this.currentShape = 'L180';
+    } else if (this.currentShape === 'L180') {
+        this.currentShape = 'L270';
+    } else if (this.currentShape === 'L270') {
+        this.currentShape = 'L';
+    }
+
+    if (this.currentShape === 'J') {
+        this.currentShape = 'J90';
+    } else if (this.currentShape === 'J90') {
+        this.currentShape = 'J180';
+    } else if (this.currentShape === 'J180') {
+        this.currentShape = 'J270';
+    } else if (this.currentShape === 'J270') {
+        this.currentShape = 'J';
+    }
+
+    if (this.currentShape === 'T') {
+        this.currentShape = 'T90';
+    } else if (this.currentShape === 'T90') {
+        this.currentShape = 'T180';
+    } else if (this.currentShape === 'T180') {
+        this.currentShape = 'T270';
+    } else if (this.currentShape === 'T270') {
+        this.currentShape = 'T';
+    }
+
+    if (this.currentShape === 'S') {
+        this.currentShape = 'S90';
+    } else if (this.currentShape === 'S90') {
+        this.currentShape = 'S';
+    }
+
+    if (this.currentShape === 'Z') {
+        this.currentShape = 'Z90';
+    } else if (this.currentShape === 'Z90') {
+        this.currentShape = 'Z';
+    }
+
+    if (this.currentShape === 'I') {
+        this.currentShape = 'I90';
+    } else if (this.currentShape === 'I90') {
+        this.currentShape = 'I';
+    }
+
+    this.currentCoor = this.shapeToCoor(this.currentShape, this.origin);
+
+    for (let i = 0; i < this.currentCoor.length; i++) {
+        if (this.currentCoor[i].col < 0 || this.currentCoor[i].col > 9) {
+            this.currentShape = prevShape;
+        }
+    }
+
+    this.currentCoor = this.shapeToCoor(this.currentShape, this.origin);
+    this.fillCells(this.currentCoor, 'blue');
+};
+
+
+tetris.currentShape = 'J';
 
 $(document).ready(function () {
     tetris.drawPlayField();
@@ -128,6 +308,8 @@ $(document).ready(function () {
             tetris.move('left');
         } else if (e.keyCode === 39) {
             tetris.move('right');
+        } else if (e.keyCode === 38) {
+            tetris.rotate();
         }
     });
 });
